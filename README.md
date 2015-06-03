0bada9f580853e3e7d457c5b511fbf164156a1a7
He Jiancheng <hejiancheng@papayamobile.com>
2015-06-03 14:09:41 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # proxy
+由于前端开发时，总是受制与后端的各种分支，导致前端维护代码非常困难，所以现在写了一个代理脚本（如附件），我们前端人员只需要在本地运行该脚本，然后配置一些相应url关联即可
+
+实现原理是创建一个完整的代理服务器，如果遇到有本地模版配置的便将数据放入到本地模板渲染。所以后台需要在套用模板的时候判断一下头部，如果有If-Appflood-Api的便吐出json数据
+
+##使用方法：
+1、本地环境需要安装python 2.7, tornado, mako
+
+2、下载附件中的脚本放到任何路径
+
+3、打开server.py配置相关的信息
+port:本地服务器端口
+api_host：需要代理的服务器地址
+api_port：需要代理的服务器地址的端口
+template_path：前端templates目录的所在路径
+static_path：前端static目录的所在路径
+
+4、在server.py目录下增加urls.txt，配置如下（key值是url的path部分，value值是对应本地的模板）：
+
+{
+	"/dsp/signin":"rtb/signin.mako",
+	"/dsp/overview":"rtb/campaign.mako"
+}
+
+5、运行server.py脚本，便开启了一个完整的代理服务器。
+http://localhost:8888/dsp/signin  访问一下，你会发现，这个url把本地的mako模关联起来了
---
+++ server.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+import sys
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+import urlparse 
+import logging,os,functools
+import simplejson as json 
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.httpclient
+from tornado.web import HTTPError, asynchronous
+from tornado.httpclient import HTTPRequest
+from tornado.options import define, options
+import mako.lookup
+import mako.template
+try:
+    from tornado.curl_httpclient import CurlAsyncHTTPClient as AsyncHTTPClient
+except ImportError:
+    from tornado.simple_httpclient import SimpleAsyncHTTPClient as AsyncHTTPClient
+relpath = lambda *a: os.path.join(os.path.dirname(__file__), *a)
+
+api_setting = open('urls.txt').read()
+define("port", default=8888, help="run on the given port", type=int)
+define("api_protocol", default="http")
+define("api_host", default="pre3.test.com")
+define("api_port", default="8989")
+define("api_setting", default=json.loads(api_setting))
+define("debug", default=True, type=bool)
+define("template_path", default=u'F:\\Appflood\\af_web\\templates\\')
+define("static_path", default=u'F:\\Appflood\\af_web\\static\\')
+
+SETTINGS={
+    "template_path":options.template_path,
+    "static_path":options.static_path,
+    "cookie_secret": "cxP6yKNYQZyhAtJ5/1gxCq1BCwD+qUnzqiHHYt2GvNY=",
+    }
+
+class ProxyHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        template_path = options.template_path #self.get_template_path()
+        
+        self.lookup = mako.lookup.TemplateLookup(directories=[template_path], input_encoding='utf-8', output_encoding='utf-8')
+
+    def render_string(self, filename, **kwargs):
+        template = self.lookup.get_template(filename)
+        namespace = self.get_template_namespace()
+        namespace.update(kwargs)
+        return template.render(**namespace)
+
+    def render(self, filename, **kwargs):
+        self.finish(self.render_string(filename, **kwargs))
+
+        
+    @asynchronous
+    def get(self):
+        # enable API GET request when debugging
+        
+        if options.debug:
+            return self.post()
+        else:
+            raise HTTPError(405)
+ 
+    @asynchronous
+    def post(self):
+        protocol = options.api_protocol
+        host = options.api_host
+        port = options.api_port
+ 
+        # port suffix
+        port = "" if port == "80" else ":%s" % port
+ 
+        uri = self.request.uri
+        url = "%s://%s%s%s" % (protocol, host, port, uri)
+ 
+        # update host to destination host
+        headers = dict(self.request.headers)
+        headers["Host"] = host
+        if self.request.path in options.api_setting.keys():
+            headers["If-Appflood-Api"] = "true"
+        try:
+            a=AsyncHTTPClient().fetch(
+                HTTPRequest(url=url,
+                            method="POST",
+                            body=self.request.body,
+                            headers=headers,
+                            follow_redirects=False),
+                self._on_proxy)
+
+        except tornado.httpclient.HTTPError, x:
+            if hasattr(x, "response") and x.response:
+                self._on_proxy(x.response)
+            else:
+                logging.error("Tornado signalled HTTPError %s", x)
+
+    
+    def _on_proxy(self, response):
+        if response.error and not isinstance(response.error,
+                                             tornado.httpclient.HTTPError):
+            raise HTTPError(500)
+        else:
+            self.set_status(response.code)
+            api_setting=options.api_setting
+            host=self.request.host
+            for header in response.headers.keys():
+                v = response.headers.get(header)
+                if header=='Location' and v:
+                    uri=list(urlparse.urlparse(v))
+                    uri[1]=host
+                    v=urlparse.urlunparse(uri)
+                if v and header!='Content-Length':
+                    self.set_header(header, v)
+            for name in api_setting.keys():
+                
+                if self.request.path==name:
+                    data=json.loads(response.body)
+                    return self.render(api_setting[name],**data)
+ 
+            if response.body:
+                self.write(response.body)
+            self.finish()
+
+
+  
+
+ 
+def main():
+    tornado.options.parse_command_line()
+    urls=[
+        (r"/images/(.*)",tornado.web.StaticFileHandler, {"path": options.static_path+'images'}),
+        (r"/js/(.*)",tornado.web.StaticFileHandler, {"path": options.static_path+'js'}),
+        (r"/css/(.*)",tornado.web.StaticFileHandler, {"path": options.static_path+'css'})
+    ]
+    urls.append((r"/.*", ProxyHandler))
+    application = tornado.web.Application(handlers =urls,**SETTINGS)
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+ 
+if __name__ == "__main__":
+    main()
